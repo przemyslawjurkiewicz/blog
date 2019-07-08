0ae54edbc49c219c57fb929bccc4e648fa9d90f2
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-07-08 10:42:46 +0200

    Add router

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/global.scss';
+import {BrowseRouter as Router} from 'react-router-dom';
 
-const Root = () => <App />;
+const Root = () => (
+  <Router>
+    <App />
+  </Router>
+);
 
 ReactDOM.render(<Root />, document.getElementById('root'));
