315577cd834b3a13dbdbcd41ef7ce21f8f39561c
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-07-23 10:41:24 +0200

    Add Nodedemon

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('./config');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,9 +9,16 @@ const app = express();
 const postRoutes = require('./routes/post.routes');
 
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use('/api', postRoutes);
+
+// connects our back end code with the database
+mongoose.connect(config.DB, { useNewUrlParser: true });
+let db = mongoose.connection;
+
+db.once('open', () => console.log('Connected to the database'));
+db.on('error', (err) => console.log('Error ' + err));
 
 app.listen(config.PORT, function() {
   console.log('Server is running on Port:', config.PORT);
