0b14c6f0c602fa68e004d483f7d966a5b032a5d7
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-07-23 10:52:30 +0200

    Add mongoDB & Helmet

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const config = require('./config');
 const mongoose = require('mongoose');
+const loadTestData = require('./testData');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -9,6 +11,7 @@ const app = express();
 const postRoutes = require('./routes/post.routes');
 
 app.use(cors());
+app.use(helmet());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use('/api', postRoutes);
@@ -17,7 +20,10 @@ app.use('/api', postRoutes);
 mongoose.connect(config.DB, { useNewUrlParser: true });
 let db = mongoose.connection;
 
-db.once('open', () => console.log('Connected to the database'));
+db.once('open', () => {
+  console.log('Connected to the database');
+  loadTestData();
+});
 db.on('error', (err) => console.log('Error ' + err));
 
 app.listen(config.PORT, function() {
