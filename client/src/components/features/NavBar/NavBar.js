1f9c44635c1c5cea8309701b4087e36c4dad2034
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-07-25 10:14:46 +0200

    Fix navabar style

--- client/src/components/features/NavBar/NavBar.js
+++ client/src/components/features/NavBar/NavBar.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import Logo from '../../common/Logo/Logo';
+import MainMenu from '../../layout/MainMenu/MainMenu';
+import './NavBar.scss';
 
 class NavBar extends React.Component {
+  state = {
+    links: [
+      {path: '/', title: 'Home'},
+      {path: '/posts/new', title: 'Add post'},
+      {path: '/posts', title: 'Posts'},
+      {path: '/contact', title: 'Contact'}
+    ]
+  };
   render() {
+    const {links} = this.state;
     return (
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/posts'>Posts</Link>
-        <Link to='/contact'>Contact</Link>
-        <Link to='/posts/new'>AddPost</Link>
-      </div>
+      <nav className='navbar'>
+        <Logo />
+        <MainMenu links={links} />
+      </nav>
     );
   }
 }
