cbd09dcd03db7abf537d46f568f4753f1663de24
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-07-25 12:17:37 +0200

    Add posRequest to redux

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -45,12 +45,25 @@ export const loadPostsRequest = () => {
   };
 };
 
-export const loadPostRequest = (id) => {
+export const loadPostRequest = id => {
   return async dispatch => {
     dispatch(startRequest());
     try {
       let res = await axios.get(`${API_URL}/posts/${id}`);
       dispatch(loadPost(res.data));
+      dispatch(endRequest());
+    } catch (e) {
+      dispatch(errorRequest(e.message));
+    }
+  };
+};
+
+export const addPostRequest = post => {
+  return async dispatch => {
+    dispatch(startRequest());
+    try {
+      let res = await axios.post(`${API_URL}/posts`, post);
+      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
       dispatch(endRequest());
     } catch (e) {
       dispatch(errorRequest(e.message));
