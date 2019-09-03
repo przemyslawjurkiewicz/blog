5397817518d68467b03d121bd3c0c5e2989c8451
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-09-03 13:59:37 +0200

    Add get post by range router

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -12,27 +12,46 @@ exports.getPosts = async (req, res) => {
 
 // get one post
 exports.getPost = async (req, res) => {
-    try {
-      res.status(200).json(await Post.findOne({id: req.params.id}));
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  };
-
-  // add new post
-exports.addPost = async function (req, res) {
-
   try {
-    const { title, author, content } = req.body;
+    res.status(200).json(await Post.findOne({id: req.params.id}));
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// add new post
+exports.addPost = async function(req, res) {
+  try {
+    const {title, author, content} = req.body;
 
     let newPost = new Post(req.body);
     newPost.id = uuid();
 
     postSaved = await newPost.save();
     res.status(200).json(postSaved);
-
-  } catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
+};
+// Get posts by range
 
-}
+exports.getPostsByRange = async function(req, res) {
+  try {
+    let {startAt, limit} = req.params;
+
+    startAt = parseInt(startAt);
+    limit = parseInt(limit);
+
+    const posts = await Post.find()
+      .skip(startAt)
+      .limit(limit);
+    const amount = await Post.countDocuments();
+
+    res.status(200).json({
+      posts,
+      amount
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -12,4 +12,7 @@ router.route('/posts/:id').get(PostController.getPost);
 // add posts
 router.route('/posts').post(PostController.addPost);
 
+//get posts by range
+router.route('/posts/range/:startAt/:limit').get(PostController.getPostsByRange)
+
 module.exports = router;
