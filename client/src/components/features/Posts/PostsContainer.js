ebd4f33a793d6b42596234230701c54f4a7108cf
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-09-04 16:59:05 +0200

    Add load post by page to container

--- client/src/components/features/Posts/PostsContainer.js
+++ client/src/components/features/Posts/PostsContainer.js
@@ -2,19 +2,21 @@ import {connect} from 'react-redux';
 import {
   getPosts,
   getRequest,
-  loadPostsRequest,
+  loadPostsByPageRequest,
+  getPages,
   resetRequest
 } from '../../../redux/postsRedux';
 import Posts from './Posts';
 
 const mapStateToProps = state => ({
   posts: getPosts(state),
-  request: getRequest(state)
+  request: getRequest(state),
+  pages: getPages(state)
 });
 
 const mapDispatchToProps = dispatch => ({
-  loadPosts: () => dispatch(loadPostsRequest()),
-  resetRequest: () => dispatch(resetRequest()),
+  loadPostsByPage: (page) => dispatch(loadPostsByPageRequest(page)),
+  resetRequest: () => dispatch(resetRequest())
 });
 
 export default connect(
